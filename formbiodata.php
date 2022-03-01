569081e1d11280cc7e54da133ef6562e74d31b81
Nurlaelaa <nurlaela6585@gmail.com>
2022-03-01 17:45:41 +0700

    first commit

---
+++ formbiodata.php
@@ -0,0 +1,11 @@
+<form method="post" action="proses_biodata.php"?>
+Nim        : <br><input type="textbox" id="Nim" name="Nim"><br>
+Nama       : <br><input type="textbox" id="Nama" name="Nama"><br>
+Kelas      : <br>
+             <input type="radio" id="Kelas" name="Kelas" value="4A">4A<br>
+             <input type="radio" id="Kelas" name="Kelas" value="4B">4B<br>
+             <input type="radio" id="Kelas" name="Kelas" value="4C">4C<br>
+             <input type="radio" id="Kelas" name="Kelas" value="4D">4D<br>
+             <br>
+<input type="submit" name="submit" value="simpan"> <input type="reset" name="reset" value="cancel">
+</form>             
---
+++ proses_biodata.php
@@ -0,0 +1,8 @@
+<?php
+echo "Nim Saya      : " .$_POST['Nim'];
+echo "<br />";
+echo "Nama Saya     : " .$_POST['Nama'];
+echo "<br />";
+echo "Kelas Saya    : " .$_POST['Kelas'];
+
+?>
